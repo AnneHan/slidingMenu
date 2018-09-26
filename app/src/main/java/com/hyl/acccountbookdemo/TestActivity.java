d142607ac3e660607ee3e1a7854db93864f6694f
AnneHan <lilyhyl@163.com>
2018-09-26 15:17:26 +0800

    TestActivity

---
+++ app/src/main/java/com/hyl/acccountbookdemo/TestActivity.java
@@ -0,0 +1,13 @@
+package com.hyl.acccountbookdemo;
+
+/**
+ * @programName: TestActivity.java
+ * @programFunction: use test
+ * @createDate: 2018/09/25
+ * @author: AnneHan
+ * @version:
+ * xx.   yyyy/mm/dd   ver    author    comments
+ * 01.   2018/09/25   1.00   AnneHan   New Create
+ */
+public class TestActivity {
+}
